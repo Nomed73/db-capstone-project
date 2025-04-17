e98c893248df1a186c4483b44744063b7a8fbaa1
Nomed73 <noe314@gmail.com>
2025-04-17 11:41:57 -0500

    feature - CancelBooking

--- AddBookingProcedure.sql
+++ AddBookingProcedure.sql
@@ -31,3 +31,21 @@ CREATE PROCEDURE UpdateBooking(
 DELIMITER ;
 
 CALL UpdateBooking(7, '2028-11-13 20:00:00');
+
+-- Task 3 - Delete a booking
+DELIMITER //
+CREATE PROCEDURE CancelBooking(IN b_id INT)
+	BEGIN
+		DECLARE o_id INT;
+        
+        SELECT orderID into o_id from Orders where bookingId = b_id;
+        
+        DELETE FROM OrderItems WHERE orderID = o_id;
+		DELETE FROM OrderDeliveryStatus WHERE orderID = o_id;
+
+		DELETE FROM Orders WHERE bookingID = b_id;
+		DELETE FROM Bookings WHERE bookingID = b_id;
+    END//
+DELIMITER ;
+
+CALL CancelBooking(4);
