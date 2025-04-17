041fa564ef0e9ef719f7ced180d6c0717d0cccc3
Nomed73 <noe314@gmail.com>
2025-04-17 11:08:55 -0500

    Add Booking Procedure

---
+++ AddBookingProcedure.sql
@@ -0,0 +1,19 @@
+-- Task 1
+
+DELIMITER //
+
+create procedure AddBooking(
+	IN bookingDate datetime, 
+    IN tableNo varchar(45), 
+    IN customerID int,
+    IN staffID int ) 
+BEGIN
+	INSERT INTO Bookings(bookingSlot, tableNo, staffID, customerID)
+	VALUES (bookingDate, tableNo, staffID, customerID);
+END//
+
+DELIMITER ;
+
+
+call AddBooking('2025-04-23 18:00:00', 'T2', 2, 5);
+
